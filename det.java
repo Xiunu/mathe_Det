78f46ff088701be7fb067efa7b75327306a07a11
Xiunu <jan@senkowski-engelhardt.de>
2018-01-22 19:03:00 +0100

    Mehr Comments
    Naja mehr Comments und groesstenteils bilingual

--- det.java
+++ det.java
@@ -11,8 +11,10 @@ public class det {
         }
         return -1;
         // bei nutzung checken ob resultat -1 falls dann return NaN
+        // in use case check if result is -1 than return NaN
     }
 
+    //Tausche Zeile mit einer anderer Zeile und gebe das resultierende Array
     //Swap one line with another and return the array
     private static double[][] swapLines(int lineSwap1, int lineSwap2, double[][] arr) {
         double[] tempSwap = arr[lineSwap2];
@@ -22,40 +24,54 @@ public class det {
     }
 
     //Berechnung mit 1. Normalform
+    //Compute the first normal form
     public static double calcDet(double[][] arr) {
 
-        // nrOfMult = 2n+n*n+1+SUMME((n-1) * n)
+        // nrOfMult = n+n*n+SUMME((n-1) * n)
 
         nrOfMult = 0;
         int nCount = arr.length;
-        //multiplier which needs to be applied to the det;
+        //multiplier which needs to be applied to the priciple diagonal
+        //multiplikator der am ende mit der Hauptdiagonelen multipliziert wird
         double curMultiplier = 1;
         double gDet = 1;
+
         //Iterate over all lines
         for (int curGaussStep = 0; curGaussStep < nCount; curGaussStep++) {
 
-            // hier eigentlich erst mal checken was denn so eine possible line ist -- und noch ein bissel error catching (ist nextLine !=-1 )
+            // find next possible row
+            // finde die nächste mögliche reihe
             int nextLine = findNextpossibleLine(curGaussStep, arr);
 
+            //catch the possibility that there is no possible line and return NaN
+            //fange die Möglichkeit ab das keine mögliche Zeile existiert und gebe NaN zurueck
             if (nextLine == -1) {
                 //herzlichen glueckwunsch das ist ein deadend!
                 System.out.println("Geht nicht, weil gibts nicht! (konnte keine passende zeile finden)");
                 return Double.NaN;
-            } else if (nextLine != curGaussStep) {
+            }
+
+            // if the next line isn't possible swap it
+            // falls mit der nächsten nicht weitergearbeitet werden kann tausche sie aus
+            else if (nextLine != curGaussStep) {
                 arr = swapLines(curGaussStep, nextLine, arr);
             }
 
-            //so hier muss es jetzt einen validen wert haben!
-            //also jetzt:
+            //so ab hier muss es jetzt einen validen wert haben
+            //at this point value needs to be valid
+
+
             //set currently operating line in currently operating column to 1 for easier operation
+            //Setze das aktuelle element auf der Hauptdiagonalen auf 1 und wende den multi auf die Komplette Zeile an
 
             curMultiplier = curMultiplier * arr[curGaussStep][curGaussStep]; //apply cur Value to multiplier
             nrOfMult++; // n mal audgefuehrt
+
             double localMulti = arr[curGaussStep][curGaussStep]; //set localMultiplier to make the first element ( !=0 ) -> 1
             for (int s = 0; s < nCount; s++) {
                 arr[curGaussStep][s] = arr[curGaussStep][s] / localMulti;
                 //apply the multiplier to the whole line
-                nrOfMult++; // n*n mal ausg
+                nrOfMult++; // n*n
             }
 
             // setting every line with a higher index than curGaussStep to zero in currently operating column by line wise subtraction
@@ -63,13 +79,9 @@ public class det {
                 double locMultiplier = arr[z][curGaussStep];
                 for (int s = curGaussStep; s < nCount; s++) {
                     arr[z][s] = arr[z][s] - locMultiplier * arr[curGaussStep][s];
-                    nrOfMult++; // n-1 nach kleinem gauss //Annahme
+                    nrOfMult++; //  Summe(n-1 * n)
                 }
             }
-        }
-        for (int i = 0; i < nCount; i++) {
-            gDet *= arr[i][i];
-            nrOfMult++; // n mal ausge
         }
         /* Printe das array
         for (int x=0; x < nCount; x++){
@@ -80,10 +92,12 @@ public class det {
             System.out.println(pS);
         }
         */
-        nrOfMult++; // 1 mal
-        return gDet * curMultiplier;
+        //since principal diagonal should be 1 in all values
+        //da die Hauptdiagonale an allen stellen eins ist kann direkt der Multiplikator als Wert zurueck gegeben werden
+        return curMultiplier;
     }
 
+    //Regel von Sarrus fuer 2x2 Matrix
     private static double sarrusDet2x2(double[][] arr) {
 
         nrOfMult=nrOfMult+2;
@@ -94,38 +108,57 @@ public class det {
     private static double[][] erstelleUntermatrix(double[][] arr, int col, int row)
     {
         //loesung inspiriert durch: https://dotnet-snippets.de/snippet/determinante-berechnen/1055
-        // erstelle
+
+        //erstelle 2-Dimensionales Array mit einer zeile und spalte weniger als das urbergebene
+        //create 2-dimensional Array with one row and col less then the overhanded array
         double[][] uMatrix = new double[arr.length-1][arr.length-1];
 
         //Da man jeweils eine bestimmte Zeile und spalte ausschließt, besteht die neue Matrix aus maximal 4 Bereichen
-        // der alten Matrix. Diese vier Bereiche werden jetzt zusammengesetzt
+        //der alten Matrix. Diese vier Bereiche werden jetzt zusammengesetzt
+
+        //Since one row and col get removed the new matrix can be created from the 4 resulting sectors, which will be merged here
 
         //Erstes Segment
+        //First Sector
+
+        //obere linke Ecke der Matrix
+        //upper left corner of the matrix
         for (int y = 0; y < col; y++)
         {
             for (int x = 0; x < row; x++)
-                //Quasi die obere linke Ecke der Matrix
                 uMatrix[x][y] = arr[x][y];
         }
+
         //Zweites Segment
+        //Second Sector
+
+        //obere rechte Ecke der Matrix
+        //upper right corner of the matrix
         for (int y = (col+1); y < arr.length; y++)
         {
             for (int x = 0; x < row; x++)
-                //obere rechte Ecke der Matrix
                 uMatrix[x][y - 1] = arr[x][y];
         }
+
         //Drittes Segment
+        //Third Sector
+
+        //untere linke Ecke der Matrix
+        //lower left corner of the matrix
         for (int y = 0; y < col; y++)
         {
             for (int x = (row + 1); x < arr.length; x++)
-                //untere linke Ecke der Matrix
                 uMatrix[x - 1][y] = arr[x][y];
         }
+
         //Viertes Segment
+        //Fourth Sector
+
+        //untere rechte Ecke der Matrix
+        //lower right corner of the matrix
         for (int y = (col + 1); y < arr.length; y++)
         {
             for (int x = (row+1); x < arr.length; x++)
-                //untere rechte Ecke der Matrix
                 uMatrix[x - 1][y - 1] = arr[x][y];
         }
 
@@ -133,6 +166,7 @@ public class det {
     }
 
     // only used to set the nrOfMult only once to 0
+    // holder dessen nutzen es ist die calcDetRec Methode aufzurufen und vorher die nrOfMult auf null zu setzten
     public static double calcDetRecHolder ( double[][] arrRec){
         nrOfMult = 0;
         return calcDetRec(arrRec);
